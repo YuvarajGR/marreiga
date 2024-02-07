fa3656e2bf73902211e76e6d821c444cdb6231fd
YuvarajGR <yuvirathod09@gamil.com>
2024-02-07 14:22:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+// for marreige
+/*
+const Bride_age = Number(prompt("Enter the Bride age: "));
+const Groom_age = Number(prompt("Enter the Groom age:"));
+
+if(Bride_age, Groom_age ){
+    if(Bride_age >=18 && Groom_age >=21){
+        console.log("Bride is elgible for marreige")
+        console.log("groom is elgible for marreige ");
+}else if(Bride_age, Groom_age){
+    if(Bride_age >=18){
+        console.log("bride is elgible for marreige");
+}
+        else if(Bride_age >=0 && Bride_age <18){
+            console.log("bride is not elgible");
+        } 
+        if (Groom_age >=21){
+            console.log("groom is elgible ");
+     }
+     else if (Groom_age >=0 && Groom_age<21){
+        console.log("groom is not elgible");
+     }
+if(Bride_age <0){
+    console.log("enter valid age of bride");
+} else if(Groom_age<0){
+    console.log("enter valid age of grom");
+}
+
+} 
+} 
+
+
+const boy_age = Number(prompt('enter boy age'));
+const girl_age = Number(prompt('enter girl age'));
+
+    if(boy_age >= 21){
+        console.log('he is eligible for marriege');
+    }else if(boy_age<=21){
+        console.log('he is not eligible for marrige');
+    }if(girl_age >=18 ){
+        console.log('she is eligible for marriege');
+    }else if(girl_age <= 18){
+        console.log('she is not eligible for marriege');
+    
+}
+*/
+
+
+//voting//
+
+/* const age = Number(prompt("Enter age for person"));
+
+if(age <18 )
+{
+    console.log("your not elgible foe vote");
+}else
+if(age >=18)
+{
+    console.log("your elgible for vote");
+} */
+
+// for Season 
+
+/* const month =Number(prompt("enter month"));
+
+if( month ===2 ||month=== 3|| month===4 || month===5){
+    alert("this is summer month "); 
+} else if( month ===6|| month===7|| month===8|| month===9){
+    alert("this is rainy month");
+} else if (month===10|| month===11|| month===12|| month===1){
+    alert("this is winter month");
+} 
+
+
+
+const no = Number(prompt('enter a no'));
+
+if(no%2 ==0){
+    alert('even no');
+}else{
+    alert('odd no');
+}
+
+
+const age = Number(prompt('enter age: '));
+
+if(age >=5){
+    const marks =Number(prompt('enter marks'));
+    if(marks >=50){
+        alert(' congrats your are eligible for admision');
+    } else {
+        alert('sorry your not eligible for admision improve your marks and come back again thank you');
+    }
+}else if(age ===4){
+    alert('come back after 1 year');
+}else if(age ===3){
+    alert('come back after 2 year');
+}else if(age ===2){
+    alert('come back after 3 year');
+}else if(age ===1){
+    alert('come back after 4 year');
+} else{
+    alert('sorry..! your not eligible for admision');
+}
+
+*/
